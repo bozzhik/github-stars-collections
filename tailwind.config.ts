d5c56c4151676569eddee9390e064fdef48485f5
maxim <bozzhik@ya.ru>
2024-11-24 17:36:22 +0300

    make `Popup` module

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,20 @@ import {Config} from 'tailwindcss'
 import {fontFamily} from 'tailwindcss/defaultTheme'
 
 const config: Config = {
-  content: ['./src/modules/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/modules/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
       sans: ['Geist', ...fontFamily.sans],
     },
     fontWeight: {
       normal: '400',
+      medium: '500',
       semibold: '600',
+    },
+    colors: {
+      background: '#0A0A0A',
+      foreground: '#e6e6e6',
+      primary: '#FEE102',
     },
     extend: {},
   },
