b1040c28ec24f10776b3d931c61630f9c9aa00c6
maxim <bozzhik@ya.ru>
2024-11-24 15:13:54 +0300

    remove `newtab, sidePanel, devTools` extension views

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,7 +1,6 @@
-import { defineManifest } from '@crxjs/vite-plugin'
+import {defineManifest} from '@crxjs/vite-plugin'
 import packageData from '../package.json'
 
-//@ts-ignore
 const isDev = process.env.NODE_ENV == 'development'
 
 export default defineManifest({
@@ -20,7 +19,6 @@ export default defineManifest({
     default_icon: 'img/logo-48.png',
   },
   options_page: 'options.html',
-  devtools_page: 'devtools.html',
   background: {
     service_worker: 'src/background/index.ts',
     type: 'module',
@@ -31,9 +29,6 @@ export default defineManifest({
       js: ['src/contentScript/index.ts'],
     },
   ],
-  side_panel: {
-    default_path: 'sidepanel.html',
-  },
   web_accessible_resources: [
     {
       resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
@@ -41,7 +36,4 @@ export default defineManifest({
     },
   ],
   permissions: ['sidePanel', 'storage'],
-  chrome_url_overrides: {
-    newtab: 'newtab.html',
-  },
 })
