93e6845bc7ea735bddc9df7ac0818144549ae731
maxim <bozzhik@ya.ru>
2024-11-24 18:31:29 +0300

    finish `button` integration into `Popup` module

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -9,8 +9,13 @@ type Props = {
 }
 
 export default function PulseButton({to, className, text}: Props) {
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault()
+    chrome.tabs.create({url: to})
+  }
+
   return (
-    <a href={to} className={cn('px-4 py-1.5 text-lg text-center font-medium text-background rounded-md bg-primary hover:bg-primary/80 duration-300 animate-buttonpulse', className)}>
+    <a href={to} onClick={handleClick} className={cn('px-4 py-1.5 text-lg text-center font-medium text-background rounded-md bg-primary hover:bg-primary/80 duration-300 animate-buttonpulse', className)}>
       {text}
     </a>
   )
--- src/modules/popup/Popup.tsx
+++ src/modules/popup/Popup.tsx
@@ -1,16 +1,35 @@
+import {useState, useEffect} from 'react'
 import HeartbeatButton from '~/UI/Button'
 
 export function Popup() {
-  const textShadow = '[text-shadow:_0_0px_10px_rgb(254_255_2_/_45%)]'
+  const USERNAME_EXAMPLE = 'bozzhik'
+  const [username, setUsername] = useState(USERNAME_EXAMPLE)
+  const [isExampleUsername, setIsExampleUsername] = useState(true)
+
+  useEffect(() => {
+    chrome.storage.sync.get(['githubUsername'], (result) => {
+      if (result.githubUsername) {
+        setUsername(result.githubUsername)
+        setIsExampleUsername(false)
+      }
+    })
+
+    chrome.storage.sync.get(null, (items) => {
+      console.log('Stored items:', items)
+    })
+  }, [])
+
+  const buttonUrl = `https://github.com/${username}?tab=stars`
+  const buttonText = isExampleUsername ? 'GitHub Stars Example' : 'Go to GitHub Stars'
 
   return (
     <main className="flex flex-col items-center gap-5 p-5 pt-4">
       <div className="flex flex-col items-center gap-0.5">
-        <h1 className={`text-3xl font-semibold text-primary text-center tracking-tight ${textShadow}`}>Github Stars Collection</h1>
+        <h1 className="text-3xl font-semibold text-primary text-center tracking-tight [text-shadow:_0_0px_10px_rgb(254_255_2_/_45%)]">Github Stars Collection</h1>
         <p className="text-base font-normal text-center">Keep your stars organized.</p>
       </div>
 
-      <HeartbeatButton to="" className="w-full" text="Go to Github Stars" />
+      <HeartbeatButton to={buttonUrl} className="w-full" text={buttonText} />
     </main>
   )
 }
